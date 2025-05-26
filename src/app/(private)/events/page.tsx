64170b3399144dd04fd2d605a3997e3bbd668ad3
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-05-26 18:02:25 +0200

    4. Events page

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -1,12 +1,45 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { db } from "@/drizzle/db";
+import { auth } from "@clerk/nextjs/server";
+import { CalendarPlus, CalendarRange } from "lucide-react";
+import Link from "next/link";
 
-const EventsPage = () => {
+const EventsPage = async () => {
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId) return redirectToSignIn();
+
+  const events = await db.query.EventTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId!),
+    orderBy: ({ createdAt }, { desc }) => [desc(createdAt)],
+  });
+
   return (
-    <div>
-      <h1>Events</h1>
-      <UserButton />
-    </div>
+    <>
+      <div className="flex gap-4 items-baseline">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
+          Events
+        </h1>
+        <Button asChild>
+          <Link href="/events/new">
+            <CalendarPlus className="mr-4 size-6" /> New Event
+          </Link>
+        </Button>
+      </div>
+      {events.length > 0 ? (
+        <h1>Events</h1>
+      ) : (
+        <div className="flex flex-col items-center gap-4">
+          <CalendarRange className="size-16 mx-auto" />
+          You do not have any events yet. Create your first event to get
+          started!
+          <Button size="lg" className="text-lg" asChild>
+            <Link href="/events/new">
+              <CalendarPlus className="mr-4 size-6" /> New Event
+            </Link>
+          </Button>
+        </div>
+      )}
+    </>
   );
 };
 
